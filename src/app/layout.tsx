144b464b4bf1ddffb6dd3bdc952fe4d56eb73458
uzumaki-ak <anikydv38@gmail.com>
2026-06-01 13:36:19 +0530

    Add Google Tag Manager snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Instrument_Sans, Plus_Jakarta_Sans, Sora, Manrope, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const instrumentSans = Instrument_Sans({
   subsets: ["latin"],
@@ -53,7 +54,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MFJGMBK2');
+          `}
+        </Script>
+      </head>
       <body className={`min-h-full flex flex-col antialiased bg-background text-foreground ${instrumentSans.variable} ${plusJakarta.variable} ${sora.variable} ${manrope.variable} ${geistSans.variable} ${geistMono.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MFJGMBK2"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           {children}
           <Toaster position="top-right" richColors />
